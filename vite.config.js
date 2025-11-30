f0aa97682f8f70bdbd1f81fca70d2b0255108d62
Benedict Maigida <teyeiesla@gmail.com>
2025-11-30 20:55:34 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,17 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  server: {
+    host: "0.0.0.0",
+    port: 5000,
+    allowedHosts: [
+      "808c94bc-75ce-46db-bd8c-76786c0dd900-00-1tfea8i0xes7w.janeway.replit.dev"
+    ],
+    hmr: {
+      clientPort: 443,
+      protocol: "wss",
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwind()],
